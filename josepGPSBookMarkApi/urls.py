a92fe4e79d39d0ad4928581cb59e262c0a842bb1
administrador <jeppalau83@gmail.com>
2019-05-13 11:10:21 +0200

    router for userviewset added

--- josepGPSBookMarkApi/urls.py
+++ josepGPSBookMarkApi/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.contrib.auth.models import User
+from rest_framework import serializers, routers, viewsets
 
-urlpatterns = {
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('rest_framework.urls'))
-
-}
+    path('api/', include(router.urls)),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
